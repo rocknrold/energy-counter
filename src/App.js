38621c6f0ada4256a3f8d5d0c2c8410820482ba5
rocknrold <bsitharoldaaron@gmail.com>
2021-09-21 15:28:24 +0800

    axie energy counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import { Card, Col, Container, Row, Button, Alert } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [energy,setEnergy] = useState(3);
+  const [round, setRound] = useState(1);
+
+
+  const handleNextRound = () => {
+    setRound(round + 1);
+    energy >= 9 ? setEnergy(10) : setEnergy(energy + 2);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Container fluid={true} className="justify-content-center">
+            <Row>
+                <Col xs={12} md={12} lg={12}>
+                    <Card>
+                        <Card.Body className={round >= 10 ? 'text-center bg-danger':'text-center'}>
+                              <Alert variant="secondary"><h1>Round {round}</h1></Alert>
+                            <h1 style={{
+                                fontSize:'12em'
+                              }}>
+                                {energy}/10
+                            </h1>
+                            <div>
+                                <Row>
+                                  <Col xs={6} lg={6} className="col col-md-6 p-2">
+                                      <Button className="btn-block w-100" variant="dark" size="lg"
+                                          onClick={() => { energy === 10 ? setEnergy(10) : setEnergy(energy + 1) }}
+                                          >
+                                          <h1>+</h1>
+                                        </Button>
+                                  </Col>
+                                  <Col xs={6} lg={6} className="col col-md-6 p-2">
+                                    <Button className="btn-block w-100" variant="dark" size="lg"
+                                        onClick={() => { energy === 0 ? setEnergy(0) : setEnergy(energy - 1) }}
+                                        ><h1>-</h1>
+                                    </Button>
+                                  </Col>
+                                  <Col xs={6} className="col col-md-6 p-2">
+                                    <Button className="btn-block w-100" variant="dark" size="lg"
+                                        onClick={handleNextRound}
+                                        ><h1>Next Round</h1>
+                                    </Button>
+                                  </Col>
+                                  <Col xs={6} className="col col-md-6 p-2">
+                                      <Button className="btn-block w-100" variant="dark" size="lg"
+                                          onClick={() => { setEnergy(3); setRound(1) }}
+                                          >
+                                          <h1>Reset</h1>
+                                        </Button>
+                                  </Col>
+                                </Row>
+                            </div>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     </div>
   );
 }
