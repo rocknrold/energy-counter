6598c7738af1bcee215e75f5e0993695535d1942
rocknrold <bsitharoldaaron@gmail.com>
2021-09-27 19:05:44 +0800

    applied react-redux toolkit for energy states

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import { Card, Col, Container, Row, Button, Alert } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { useState } from 'react';
+// import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { decrement, increment, nextRound, reset} from './redux/counterSlice';
 
 function App() {
 
-  const [energy,setEnergy] = useState(3);
-  const [round, setRound] = useState(1);
+  const energy = useSelector((state) => state.counter.energy)
+  const round = useSelector((state) => state.counter.round)
+  const dispatch = useDispatch()
 
 
-  const handleNextRound = () => {
-    setRound(round + 1);
-    energy >= 9 ? setEnergy(10) : setEnergy(energy + 2);
-  }
+
+  // const [energy,setEnergy] = useState(3);
+  // const [round, setRound] = useState(1);
+
+
+  // const handleNextRound = () => {
+  //   setRound(round + 1);
+  //   energy >= 9 ? setEnergy(10) : setEnergy(energy + 2);
+  // }
 
 
   return (
@@ -32,26 +41,30 @@ function App() {
                                 <Row>
                                   <Col xs={6} lg={6} className="col col-md-6 p-2">
                                       <Button className="btn-block w-100" variant="dark" size="lg"
-                                          onClick={() => { energy === 10 ? setEnergy(10) : setEnergy(energy + 1) }}
+                                          // onClick={() => { energy === 10 ? setEnergy(10) : setEnergy(energy + 1) }}
+                                          onClick={() => { dispatch(increment()) }}
                                           >
                                           <h1>+</h1>
                                         </Button>
                                   </Col>
                                   <Col xs={6} lg={6} className="col col-md-6 p-2">
                                     <Button className="btn-block w-100" variant="dark" size="lg"
-                                        onClick={() => { energy === 0 ? setEnergy(0) : setEnergy(energy - 1) }}
+                                        // onClick={() => { energy === 0 ? setEnergy(0) : setEnergy(energy - 1) }}
+                                        onClick={() => { dispatch(decrement()) }}
                                         ><h1>-</h1>
                                     </Button>
                                   </Col>
                                   <Col xs={6} className="col col-md-6 p-2">
                                     <Button className="btn-block w-100" variant="dark" size="lg"
-                                        onClick={handleNextRound}
+                                        // onClick={handleNextRound}
+                                        onClick={() => { dispatch(nextRound()) }}
                                         ><h1>Next Round</h1>
                                     </Button>
                                   </Col>
                                   <Col xs={6} className="col col-md-6 p-2">
                                       <Button className="btn-block w-100" variant="dark" size="lg"
-                                          onClick={() => { setEnergy(3); setRound(1) }}
+                                          // onClick={() => { setEnergy(3); setRound(1) }}
+                                          onClick={() => { dispatch(reset()) }}
                                           >
                                           <h1>Reset</h1>
                                         </Button>
---
+++ src/redux/counterSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    energy : 0,
+    round: 1,
+}
+
+export const counterSlice = createSlice({
+    name:'counter',
+    initialState,
+    reducers: {
+        increment: (state) => {
+            state.energy >= 10 ? state.energy = 10 : state.energy += 1
+        },
+        decrement: (state) => {
+            state.energy <= 0 ? state.energy = 0 : state.energy -= 1
+        },
+        nextRound: (state) => {
+            state.round += 1
+            state.energy >= 9 ? state.energy = 10 : state.energy += 2
+        },
+        reset: (state) => {
+            state.round = 1
+            state.energy = 3
+        }
+    }
+})
+
+export const { increment, decrement, nextRound, reset } = counterSlice.actions
+
+export const setEnergy = state => state.counter.energy
+export const setRound = state => state.counter.round
+
+export default counterSlice.reducer
